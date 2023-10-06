173c69651167656e63de0a508cdc975adf27d44b
sabaghhh <22-101257@students.eui.edu.eg>
2023-10-06 17:16:21 +0300

    a

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print value of n satus; positive, negative or zero
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int c = 0;
+	for (c<10)
+	{
+		printf("%i", c);
+		c++;
+	}
+	printf("\n");
+
+	return (0);
+}
