83e5bc94d91f8d12b62b39f99e720bcbabfec236
sabaghhh <22-101257@students.eui.edu.eg>
2023-10-10 19:03:05 +0300

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+    printf("_putchar\n");
+    return 0;
+}
+
