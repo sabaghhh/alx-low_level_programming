d7adce58712af9b5c09ecc025139cdda3ca713dd
sabaghhh <22-101257@students.eui.edu.eg>
2023-10-16 22:24:37 +0300

    a

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/**
+ * swap_int - swaps the values of two integers
+ *		using two input parameters
+ *
+ * @a: input parameter 1
+ * @b: input parameter 2
+ *
+ * Return: Nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int t;
+
+	t = *a;
+	*a = *b;
+	*b = t;
+}
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int length, index;
+	char character;
+
+	for (length = 0; s[length] != '\0'; ++length)
+	{
+		;
+	}
+	for (index = 0; index < length / 2; ++index)
+	{
+		character = s[index];
+		s[index] = s[length - 1 - index];
+		s[length - 1 - index] = character;
+	}
+
+}
