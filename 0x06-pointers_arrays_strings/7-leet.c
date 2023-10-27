f2b496f69f1d3109d04748de5cf34c0d85cbd964
sabaghhh <22-101257@students.eui.edu.eg>
2023-10-28 01:31:22 +0200

    libmy

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,26 +3,23 @@
 /**
  * leet - encodes a string in 1337
  * @s: string to be encoded
- *
+ * Description: this is good
  * Return: the resulting string;
  */
-char *leet(char *s)
+char *_strcat(char *s)
 {
-	int i, j;
-
-	char *a = "aAeEoOtTlL";
-	char *b = "4433007711";
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			if (s[i] == a[j])
-			{
-				s[i] = b[j];
-			}
-		}
-	}
-
-	return (s);
+for (int i = 0 ; s[i] != '\0' ; i++)
+{
+if (s[i] == 'a' || s[i] == 'A')
+s[i] = '4';
+else if (s[i] == 'e' || s[i] == 'E')
+s[i] = '3';
+else if (s[i] == 'o' || s[i] == 'O')
+s[i] = '0';
+else if (s[i] == 't' || s[i] == 'T')
+s[i] = '7';
+else if (s[i] == 'l' || s[i] == 'L')
+s[i] = '1';
+}
+return (s);
 }
